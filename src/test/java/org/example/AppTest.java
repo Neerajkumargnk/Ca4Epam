9e9d729c1e3ef5b903e03521f8eccd897268e94d
neeraj_12 <neerajkumargnk12@gmail.com>
2023-04-21 10:12:55 +0530

    All completed

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,62 @@
+package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+public class AppTest{
+    WebDriver driver;
+    @BeforeClass
+    @Parameters({"browser","url"})
+    void setUpDriver(String browser, String link) throws InterruptedException {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        driver.get(link);
+        driver.manage().window().maximize();
+        Thread.sleep(10000);
+    }
+    @Test
+    void dropdown1() {
+        WebElement DropDown = driver.findElement(By.id("dropdowm-menu-1"));
+        Select dropdown = new Select(DropDown);
+        dropdown.selectByVisibleText("Python");
+    }
+    @Test
+    void dropdown2() {
+        WebElement DropDown = driver.findElement(By.id("dropdowm-menu-2"));
+        Select dropdown = new Select(DropDown);
+        dropdown.selectByVisibleText("TestNG");
+    }
+    @Test
+    void dropdown3() {
+        WebElement DropDown = driver.findElement(By.id("dropdowm-menu-3"));
+        Select dropdown = new Select(DropDown);
+        dropdown.selectByVisibleText("JavaScript");
+    }
+    @Test
+    void checkbox1() {
+        driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/label[2]/input")).click();
+    }
+    @Test
+    void checkbox2() {
+        driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/label[3]/input")).click();
+    }
+
+    @Test
+    void radioButton() {
+        driver.findElement(By.xpath("  //*[@id=\"radio-buttons\"]/input[3]")).click();
+}
+}
